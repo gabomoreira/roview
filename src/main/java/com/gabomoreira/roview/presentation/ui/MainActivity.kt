30c133937bb83ac487eb496bb5d3798225f995fa
Gabriel Moreira <gsm.br2003@gmail.com>
2024-05-22 21:36:08 -0300

    chore: config packages

---
+++ src/main/java/com/gabomoreira/roview/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.gabomoreira.roview.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gabomoreira.roview.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
